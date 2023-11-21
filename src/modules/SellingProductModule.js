786bb22edcfa6e20498a33eddfd5df9961fef8ac
김민범 <minbum0501@naver.com>
2023-11-21 15:39:53 +0900

    [FE][FEATURE] 상품등록API

--- src/modules/SellingProductModule.js
+++ src/modules/SellingProductModule.js
@@ -1,26 +1,33 @@
 import { createActions, handleActions } from 'redux-actions';
+import { POST_PRODUCT } from '../path/to/actions';
 
 /* 초기값 */
-const initialState = [];
+const initialState = {
+  product: null,
+  products: [],
+  mealProducts: [],
+  dessertProducts: [],
+  beverageProducts: [],
+};
 
 /* 액션 */
-export const GET_PRODUCT = 'product/GET_PRODUCT';
-export const GET_PRODUCTS = 'product/GET_PRODUCTS';
-export const GET_PRODUCTS_MEAL = 'product/GET_PRODUCTS_MEAL';
-export const GET_PRODUCTS_DESSERT = 'product/GET_PRODUCTS_DESSERT';
-export const GET_PRODUCTS_BEVERAGE = 'product/GET_PRODUCTS_BEVERAGE';
-export const POST_PRODUCT = 'product/POST_PRODUCT';
-export const PUT_PRODUCT = 'product/PUT_PRODUCT';
-
-const actions = createActions(
+export const {
+  getProduct,
+  getProducts,
+  getProductsMeal,
+  getProductsDessert,
+  getProductsBeverage,
+  postProduct,
+  putProduct,
+} = createActions(
   {
-    [GET_PRODUCT]: () => {},
-    [GET_PRODUCTS]: () => {},
-    [GET_PRODUCTS_MEAL]: () => {},
-    [GET_PRODUCTS_DESSERT]: () => {},
-    [GET_PRODUCTS_BEVERAGE]: () => {},
-    [POST_PRODUCT]: () => {},
-    [PUT_PRODUCT]: () => {},
+    GET_PRODUCT: (product) => ({ product }),
+    GET_PRODUCTS: (products) => ({ products }),
+    GET_PRODUCTS_MEAL: (mealProducts) => ({ mealProducts }),
+    GET_PRODUCTS_DESSERT: (dessertProducts) => ({ dessertProducts }),
+    GET_PRODUCTS_BEVERAGE: (beverageProducts) => ({ beverageProducts }),
+    POST_PRODUCT: (product) => ({ product }),
+    PUT_PRODUCT: (product) => ({ product }),
   },
   { prefix: 'product' } // 액션 타입에 접두사 추가
 );
@@ -28,13 +35,13 @@ const actions = createActions(
 /* 리듀서 */
 const productReducer = handleActions(
   {
-    [GET_PRODUCT]: (state, { payload }) => payload,
-    [GET_PRODUCTS]: (state, { payload }) => payload,
-    [GET_PRODUCTS_MEAL]: (state, { payload }) => payload,
-    [GET_PRODUCTS_DESSERT]: (state, { payload }) => payload,
-    [GET_PRODUCTS_BEVERAGE]: (state, { payload }) => payload,
-    [POST_PRODUCT]: (state, { payload }) => payload,
-    [PUT_PRODUCT]: (state, { payload }) => payload,
+    [GET_PRODUCT]: (state, { payload }) => ({ ...state, product: payload.product }),
+    [GET_PRODUCTS]: (state, { payload }) => ({ ...state, products: payload.products }),
+    [GET_PRODUCTS_MEAL]: (state, { payload }) => ({ ...state, mealProducts: payload.mealProducts }),
+    [GET_PRODUCTS_DESSERT]: (state, { payload }) => ({ ...state, dessertProducts: payload.dessertProducts }),
+    [GET_PRODUCTS_BEVERAGE]: (state, { payload }) => ({ ...state, beverageProducts: payload.beverageProducts }),
+    [POST_PRODUCT]: (state, { payload }) => ({ ...state, product: payload.product }),
+    [PUT_PRODUCT]: (state, { payload }) => ({ ...state, product: payload.product }),
   },
   initialState
 );
