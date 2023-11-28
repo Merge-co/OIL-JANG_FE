7f97ce3beef0c26854780188e238903628a30cd5
Somangly <105614779+somang1206@users.noreply.github.com>
2023-11-28 14:55:39 +0900

    [FE][FEATURE] 쪽지 모달 진행중

--- src/pages/message/MessageList.js
+++ src/pages/message/MessageList.js
@@ -11,22 +11,32 @@ import PagingBar from '../../components/common/PagingBar';
 import ButtonCSS from '../../styles/Button.module.css';
 import MessageMenu from '../../components/message/MessageMenu';
 import MessageListCSS from '../../styles/message/MessageList.module.css'
+import { GET_PAGING } from '../../modules/PagingModule';
+import { jwtDecode } from 'jwt-decode';
+import { getCookie } from '../../modules/CookieModule';
 
-function MessageList({isReceived}){
+function MessageList({isReceived, keyword, page}){
 
 
     const navigate = useNavigate();
     const PagingInfo = useSelector(state => state.pagingReducer);
     const dispatch = useDispatch();
     const params = useParams();
-    const messages = useSelector(state => state.messageReducer);
+    //const {page, keyword} = params;
+    //const messages = useSelector(state => state.messageReducer);
    // const messageList = messages.data;
 
+   const curURL = new URL(window.location.href);
 
-    const [messageList, setMessageList] = useState([]);
 
-    //let msgList = messages[0] ? messages[0].msgList : "";
-    let pagingBtn = messages[0] ? messages[0].pagingBtn : "";
+    const [messageList, setMessageList] = useState();
+    let msgList = messageList && messageList[0].msgList;
+    console.log("1" + JSON.stringify(msgList))
+    let pagingBtn = messageList && messageList[0].pagingBtn;
+    console.log("2" + JSON.stringify(pagingBtn))
+    let totalMsg = messageList && messageList[0].totalMsg;
+    console.log("pagingBtn ==================" + pagingBtn)
+    console.log("messageList ==================" + messageList)
 
     const handleMessageClick = (msgCode) => {
         navigate(`/messageDetail/${msgCode}`);
@@ -38,13 +48,19 @@ function MessageList({isReceived}){
     useEffect(
         () => {
             dispatch(callMessageListAPI({
-                userCode:1,
+                userCode: jwtDecode(getCookie("accessToken")).userCode,
                 isReceived: true,
-
+                page: page,
+                keyword: keyword,
             })).then((result) => {
+                console.log("page============"+page)
+
                 console.table("result : " + result);
                 if(result && result.data){
-                    setMessageList([...result.data]);
+                    setMessageList([result.data.results]);
+                    if(!curURL.searchParams.get('page')){
+                        dispatch({type: GET_PAGING, payload: 0});
+                    }
                 }else{
                     console.error('[MessageList] API response does not contain data:', result);
                 }
@@ -53,20 +69,28 @@ function MessageList({isReceived}){
                 console.error('[MessageList] API call error:', error);
             })
         }
-        ,[PagingInfo, dispatch, isReceived]
+        ,[PagingInfo, dispatch, isReceived, page, keyword]
     );
+
+    console.log('messageList:==========================', messageList);
+
 
     const handleMenuClick = useCallback((isReceived) => {
             // 메뉴 클릭에 대한 동작 정의
             if (isReceived === 'true') {
                 // 받은 쪽지함 처리
                 dispatch(callMessageListAPI({
-                userCode: 1,
+                userCode: jwtDecode(getCookie("accessToken")).userCode,
                 isReceived: true,
+                page: page,
+                keyword: keyword,
                 })).then((result) => {
                 console.table("result : " + result);
                 if(result && result.data){
-                    setMessageList([...result.data]);
+                    setMessageList([result.data.results]);
+                    if(!curURL.searchParams.get('page')){
+                        dispatch({type: GET_PAGING, payload: 0});
+                    }
                 }else{
                     console.error('[MessageList] API response does not contain data:', result);
                 }
@@ -78,12 +102,17 @@ function MessageList({isReceived}){
             } else if (isReceived === 'false') {
                 // 보낸 쪽지함 처리
                 dispatch(callMessageListAPI({
-                userCode: 1,
+                userCode: jwtDecode(getCookie("accessToken")).userCode,
                 isReceived: false,
+                page: page,
+                keyword: keyword,
                 })).then((result) => {
                 console.table("result : " + result);
                 if(result && result.data){
-                    setMessageList([...result.data]);
+                    setMessageList([result.data.results]);
+                    if(!curURL.searchParams.get('page')){
+                        dispatch({type: GET_PAGING, payload: 0});
+                    }
                 }else{
                     console.error('[MessageList] API response does not contain data:', result);
                 }
@@ -92,7 +121,7 @@ function MessageList({isReceived}){
                 console.error('[MessageList] API call error:', error);
             })
             };
-                }, [dispatch]); 
+                }, [PagingInfo, dispatch, isReceived, page, keyword]); 
         
         
 
@@ -137,6 +166,7 @@ function MessageList({isReceived}){
                 console.log("msgCode 전송: " + msgCode)
               alert("쪽지 삭제에 성공했습니다!");
               navigate(`/messageList`, { replace: false });
+              window.location.reload();
             });
           } else {
             alert("삭제할 쪽지를 선택해주세요");
@@ -144,32 +174,32 @@ function MessageList({isReceived}){
           }
         };
 
-
+  ;
 
 
 
     return (
         <>
-            
-            <h1>쪽지함</h1>
-            <hr/>
+            <div style={{width: '70%', margin: '0 auto'}}>
+                <h1>쪽지함</h1>
+                <hr />
+            </div>
             <div className={`${MessageListCSS.box1} ${MessageListCSS.contBox}`}>
 
                 <div className={`${MessageListCSS.msgNav}`}>
                {memoizedMessageMenu}
                 </div>
 
-                <div>
+                <div style={{width: '80%', margin: '0 auto'}}>
                     <div>
                         <form action='' method='get' className={`${MessageListCSS.searchBox}`}>
                             <select name='search' className={`${MessageListCSS.search}`}>
                                 <option value="전체">전체</option>
-                                <option value="제목">제목</option>
+                                <option value="제목">내용</option>
                                 <option value="이름">이름</option>
-                                <option value="분류">분류</option>
                             </select>
-                            <input type="search" placeholder="검색" className={`${MessageListCSS.searchInput}`}/>
-                            <input type="submit" value="검색" className={`${MessageListCSS.searchBtn}`}/>
+                            <input type="search" placeholder="검색" className={`${MessageListCSS.searchInput}`} name="keyword" defaultValue={keyword}/>
+                            <input type="submit" value="검색" className={`${MessageListCSS.searchBtn}`} style={{cursor:'pointer'}}/>
                         </form>
                     
                  
@@ -187,24 +217,32 @@ function MessageList({isReceived}){
                             
                             </thead>
                             <tbody>
-                                {messageList && messageList.map(message => (
+                                {messageList && messageList[0].msgList.map(message => (
                                     <tr key={message.msgCode}>
                                         <td><input type="checkbox" name="sort" id={message.msgCode} onChange={handleCheckedOne}/></td>
                                         <td>{`${message.name} ${message.id}`}</td>
-                                        <td onClick={() => handleMessageClick(message.msgCode)} style={{cursor:'pointer'}} className={`${MessageListCSS.msgListHover}`}>{message.msgContent}</td>
+                                        <td onClick={() => handleMessageClick(message.msgCode)} style={{cursor:'pointer'}} className={`${MessageListCSS.msgListHover}`}>
+                                            {message.msgContent.length > 10 ? `${message.msgContent.substring(0,10)}...` : message.msgContent}
+                                        </td>
                                         <td>{message.msgTime}</td>
                                         <td>{message.msgStatus}</td>
                                     </tr>
                                 ))}
+
+                            {console.log("messageList3333 " + messageList)}
                        
                             </tbody>
             
                         </table>
 
                         <input type="submit" value="삭제" className={`${ButtonCSS.middleBtn2}`} style={{float: 'right'}} onClick={() => onDeleteHandler(messageList.msgCode)}/>
+                        
 
-                        {messageList ? <PagingBar pagingBtn={pagingBtn}/> : ""}
-          
+                        {(totalMsg && messageList.length === totalMsg) ? "" : messageList ? <PagingBar pagingBtn={pagingBtn}/> : ""}
+
+                        {console.log("pagingBtn " + pagingBtn)}
+                        {console.log("messageList " + messageList)}
+                                  
                     </div>
           
                 </div>   
