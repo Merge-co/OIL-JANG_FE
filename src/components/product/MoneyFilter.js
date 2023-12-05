6e0b519c36ee8ff0f35ed87f554576841d1bc0a3
ksostuft <ksostuft@gmail.com>
2023-12-05 19:10:59 +0900

    20231205-product-010 input에서 searchParam 있으면 들어감

--- src/components/product/MoneyFilter.js
+++ src/components/product/MoneyFilter.js
@@ -4,11 +4,14 @@ import ProductFilterCSS from '../../styles/product/ProductFilter.module.css'
 import { useDispatch } from 'react-redux';
 import { GET_MONEY_SETTING } from '../../modules/ProductModule';
 function MoneyFilter() {
+    const curURL = new URL(window.location.href);
 
     const [ moneyCriteria, setMoneyCriteria ] = useState({
-        minPriceValue: '',
-        maxPriceValue: ''
+        minPriceValue: curURL.searchParams.get('minPrice') || '',
+        maxPriceValue: curURL.searchParams.get('maxPrice') || ''
     });
+
+    console.log( curURL.searchParams.get('minPrice'))
 
     const { minPriceValue, maxPriceValue } = moneyCriteria;
 
